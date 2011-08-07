f43c43201cb68ce801dec75602ca0b84ff5cae0a
prabesh <prabesh@prabesh.(none)>
2011-08-07 11:03:12 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crickytube::Application.config.secret_token = 'b8517720bf4f30347b858bb1ffe0284a369f03eea081a2a783e3e54f909b9bb77a582849fc52578537a64ec3c110bd3fb23981c2eb1a56793b64e8f3e88466b9'
