30e0b401dc2927a96595135e6e72e4be64401a3c
prabesh <prabesh@prabesh.(none)>
2011-08-07 11:26:18 -0500

    added title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,26 @@
 class PagesController < ApplicationController
   def recent
+  	@title ="recent"
   end
 
   def popular
+ 	@title ="popular"
   end
 
   def crickytube
+ 	@title = "crickytube"
   end
 
   def fanfights
+	@title ="fanfights"
   end
 
   def livelinks
+	@title ="livelinks"
   end
 
   def highlights
+	@title ="highlights"
   end
 
 end
